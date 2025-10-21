76b49dc010e699a9441bade2acdb2660af0e887a
hamzazmah123 <hamza.g23266@iqra.edu.pk>
2025-10-21 19:42:18 +0500

    Backend Update Timeline Addedd!!

--- Backend/controllers/timelineController.js
+++ Backend/controllers/timelineController.js
@@ -2,33 +2,62 @@ import { catchAsyncErrors } from "../middlewares/catchAsyncErrors.js";
 import ErrorHandler from "../middlewares/error.js";
 import { Timeline } from "../models/timelineSchema.js";
 
-export const postTimeline = catchAsyncErrors(async(req, res, next) => {
-    const {title, description, from, to} = req.body || {};
-    const timelineNew = await Timeline.create({title, description, timeline: {from, to},});
-    res.status(200).json({
-        success: true,
-        message: "Timeline Added Successfully!!",
-        timelineNew,
-    })
+export const postTimeline = catchAsyncErrors(async (req, res, next) => {
+  const { title, description, from, to } = req.body || {};
+  const timelineNew = await Timeline.create({
+    title,
+    description,
+    timeline: { from, to },
+  });
+  res.status(200).json({
+    success: true,
+    message: "Timeline Added Successfully!!",
+    timelineNew,
+  });
 });
 
-export const deleteTimeline = catchAsyncErrors(async(req, res, next) => {
-    const { id } = req.params;
-    const timeline = await Timeline.findById(id); 
-    if(!timeline) {
-        return next(new ErrorHandler("Timeline Did not Exist!!", 404));
+export const deleteTimeline = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  const timeline = await Timeline.findById(id);
+  if (!timeline) {
+    return next(new ErrorHandler("Timeline Did not Exist!!", 404));
+  }
+  await timeline.deleteOne();
+  res.status(200).json({
+    success: true,
+    message: "Timeline Deleted Successfully!!!",
+  });
+});
+
+export const updateTimeline = catchAsyncErrors(async (req, res, next) => {
+  const { id } = req.params;
+  let timeline = await Timeline.findById(id);
+  if (!timeline) {
+    return next(new ErrorHandler("Timeline did not exist!!!", 404));
+  }
+
+  const { title, description, from, to } = req.body || {};
+  timeline = await Timeline.findByIdAndUpdate(
+    id,
+    { title, description, timeline: { from, to } },
+    {
+      new: true,
+      runValidators: true,
+      useFindAndModify: false,
     }
-    await timeline.deleteOne();
-    res.status(200).json({
-        success: true,
-        message: "Timeline Deleted Successfully!!!",
-    });
+  );
+
+  res.status(200).json({
+    success: true,
+    message: "Timeline updated Successfully!!",
+    timeline,
+  });
 });
 
-export const getAllTimelines = catchAsyncErrors(async(req, res, next) => {
-    const timelines = await Timeline.find();
-    res.status(200).json({
-        success: true,
-        timelines,
-    });
+export const getAllTimelines = catchAsyncErrors(async (req, res, next) => {
+  const timelines = await Timeline.find();
+  res.status(200).json({
+    success: true,
+    timelines,
+  });
 });
